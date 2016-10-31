5893fa03dafc4d0a7a2c2e86f459a0a2eabd9c30
guillaume.garcia@telecomnancy.net <guillaume.garcia@telecomnancy.net>
2016-10-31 02:38:05 +0100

    Début du coding en C#. Ajout de AUTHORS.

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 Un gros clicker mamene
 
-ça fait du sale waaaaaaaaaaaaai
+ça fait du sale waaaaaaaaaaaaai ouais^^
 
 
 <h1> Printer </h1>
 
 <p> Plus tu cliques plus tu te fais du fric ! </p> 
+
+
+Version
+
+	0.0.0.1
---
+++ mainclass.cs
@@ -0,0 +1,18 @@
+using System;
+ 
+namespace mainclass
+{
+ public class mainclass
+ {
+ 	public mainclass()
+ 	{
+ 
+ 	}
+ 
+ 	public static void Main(string[] args)
+ 	{
+ 		Console.WriteLine("ouais^^ ");
+ 
+ 	}
+ }
+}
